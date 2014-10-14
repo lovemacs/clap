1d4131deecf79f823119ec6c0bcbf283d825b0e3
liango2 <540625404@qq.com>
2014-10-14 17:35:11 +0800

    draw frame 2

--- src/main/clojure/frame.clj
+++ src/main/clojure/frame.clj
@@ -2,11 +2,17 @@
   (:import (javax.swing JFrame JPanel JTextArea JScrollPane)
            (java.awt GridLayout)))
 
-(def frame (JFrame. "title"))         (.setSize frame 300 300)
-(def panel (JPanel.))                  (.setLayout panel (GridLayout.))
-(def textArea (JTextArea.))            (.setText textArea  "test")
+;(def frame (JFrame. "title"))         (.setSize frame 300 300)
+;(def panel (JPanel.))                  (.setLayout panel (GridLayout.))
+;(def textArea (JTextArea.))            (.setText textArea  "test")
+;
+;(.add panel (JScrollPane. textArea))
+;(.add frame panel)
+;
+;(.setVisible frame true)
 
-(.add panel (JScrollPane. textArea))
-(.add frame panel)
-
-(.setVisible frame true)
+(def frame (JFrame. "Title"))
+(def panel (JPanel.))
+(def textArea (doto (JTextArea.) (.setText "test")) )
+(doto panel (.setLayout (GridLayout.)) (.add (JScrollPane. textArea)))
+(doto frame (.setSize 300 300) (.add panel) (.setVisible true))
