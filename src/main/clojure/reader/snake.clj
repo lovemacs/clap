6603956e5ad6e7b5db71f67555a434fe05d36f79
liango2 <540625404@qq.com>
2014-10-14 17:57:20 +0800

    snake change

--- src/main/clojure/reader/snake.clj
+++ src/main/clojure/reader/snake.clj
@@ -1,5 +1,7 @@
 (ns reader.snake
-  (:import (java.awt.event ActionListener))
+  (:import (java.awt.event ActionListener)
+           (javax.swing JTextArea JScrollPane)
+           (java.awt GridLayout))
   (:import (java.util Date Calendar)
            (java.net URI ServerSocket)
            java.sql.DriverManager
@@ -7,19 +9,28 @@
            (javax.swing JPanel JFrame Timer JOptionPane)
            (java.awt.event ActionListener KeyListener))
   (:use examples.import-static))
+
 (import-static java.awt.event.KeyEvent VK_LEFT VK_RIGHT VK_UP VK_DOWN)
 
-
+;;; test
 (def *now* (Date.))
 (println *now*)
 
-(def frame (JFrame. "xxx"))
-(.setSize frame 400 400)
-(.setVisible frame true)
 
 
 ;(proxy [JPanel] []
 ;  (paintComponent [g]
 ;    (proxy-super paintComponet g)))
 
+(defn game []
+  (let [frame (JFrame. "Title3") panel (JPanel.) textArea (JTextArea.)]
+    (doto textArea (.setText "test"))
+    (doto panel (.setLayout (GridLayout.)) (.add (JScrollPane. textArea)))
+    (doto frame (.setSize 300 300) (.add panel) (.setVisible true))
 
+    ;[frame panel textArea]
+    )
+  )
+
+;;;test
+(game)
